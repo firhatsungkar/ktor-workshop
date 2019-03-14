ca3a7c1fae2db331c94956ee0b58cd4ba736bb85
Deny Prasetyo <jasoet87@gmail.com>
2019-03-14 17:24:41 +0700

    Project for Ktor Workshop

---
+++ rest-db/alumni-news-db-05-controller/src/main/kotlin/com/packtpub/alumni/model/University.kt
@@ -0,0 +1,26 @@
+package com.packtpub.alumni.model
+
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "city")
+data class City(
+        @Id
+        var id: String,
+        var countryCode: String,
+        var name: String,
+        var areaCode: Int?
+)
+
+@Entity
+@Table(name = "university")
+data class University(
+        @Id
+        var id: String,
+        var city: City,
+        var name: String,
+        var address: String,
+        var webPage: String?
+)
